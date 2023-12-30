84e18d7dce62fab909c1a687e812554cfc277dc6
ordinis1709 <minhnguyenquang2004vn@gmail.com>
2023-12-31 03:31:50 +0700

    The 1st commit. Add WorkSession.java and  textFile.java

---
+++ WorkSession.java
@@ -0,0 +1,58 @@
+import java.io.IOException;
+// import java.util.Scanner;//user input
+
+
+public class WorkSession {
+	public static String id;
+	public static String color;
+	public static String text;
+	public static String dir;
+
+
+	public static void main(String[] args) {
+    	// Create note1.txt
+    	text = "new bitch";
+    	textFile note1 = new textFile();
+    	//try {
+        	if (note1.createNote("My Title", text)) {
+            	System.out.println("Note1 is created successfully!");
+        	} else {
+            	System.out.println("Failed!");
+        	}
+        	/*note1.getWriter().close();
+        	if(note1.getWriter() != null){
+   			System.out.println("Closing note1 failed!--------");
+   		}*/
+    	//} catch (IOException e) {
+        //	System.out.println("There's an error!");
+        //	e.printStackTrace();
+    	//}
+
+
+    	// String yourName;
+    	// Scanner myScanner = new Scanner(System.in);
+    	// System.out.println("Enter username");
+    	// yourName = myScanner.nextLine();
+    	// System.out.println("Your name is "+ yourName);
+
+
+    	System.out.println("The id of note1 is: " + note1.getId());
+
+
+    	// Create note2.txt
+    	text = "Hello, this is new note2!";
+    	textFile note2 = new textFile();
+    	//try {
+   	 if(note2.createNote("Note2 title", text)){
+            	System.out.println("Note2 is created successfully!");
+        	} else {
+            	System.out.println("Failed!");
+        	}
+        	//note2.getWriter().close();
+    	//} catch (IOException e) {
+        //	System.out.println("There's an error!");
+        //	e.printStackTrace();
+    	//}
+    	System.out.println("The id of note2 is: " + note2.getId());
+	}
+}
---
+++ textFile.java
@@ -0,0 +1,84 @@
+import java.io.*;
+import java.io.IOException;
+import java.lang.InterruptedException;
+
+
+public class textFile extends File {
+	public static int id = 1;
+	public String color = "white";
+	public String title;
+	private int pId = id++;
+	private String dir = "myNotes/note" + pId + ".txt";
+
+
+	public int getId() {
+    	return this.pId;
+	}
+
+
+	private String content;
+	private boolean lock = false;
+
+
+	private FileWriter myWriter;
+
+
+	public FileWriter getWriter() {
+    	return this.myWriter;
+	}
+
+
+	public void setStatus(boolean lock) {
+    	if (true) // security check, remember to add
+    	{
+        	this.lock = lock;
+    	}
+	}
+
+
+	public String getContent() {
+    	if (this.lock) {
+        	return null;
+    	}
+    	return this.content;
+	}
+
+
+	public void setContent(String text) {
+    	this.content = text;
+	}
+
+
+	// The File class does not have a default constructor->need to call inside of
+	// the instance class
+	// Constructor of temporary file
+	public textFile() {
+    	// String adress;
+    	super("myNotes");
+    	this.mkdir();
+	}
+
+	// Save the file
+	public boolean createNote(String title, String text) {
+    	String newName = this.dir;
+    	try{
+   		/*if(this.myWriter != null){
+   			System.out.println("myWriter.close() failed!--------");
+   		}*/
+    		/*try{
+   	 		while(this.myWriter != null) {
+       			Thread.sleep(100); // Wait for 100 milliseconds
+   		}*/
+        	//this.delete();
+        	FileWriter myWriter = new FileWriter(newName);
+        	this.setContent(text);
+        	this.title = title;
+        	myWriter.write(text);
+        	myWriter.close();
+    	} catch (IOException e) {
+        	System.out.println("An error occurred.");
+        	e.printStackTrace();
+    	}
+    	return true;
+	}
+}
